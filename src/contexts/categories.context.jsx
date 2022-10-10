21364ef2e7c0ceaaf6a0a569f5eaeb25b518ab5e
DiegoBezerra89 <diegobmartins21@gmail.com>
2022-10-10 08:12:07 -0300

    Applied Apollo and GraphQL

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,23 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+import { gql, useQuery } from "@apollo/client";
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -9,16 +26,26 @@ export const CategoriesContext = createContext({
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  const { loading, data } = useQuery(COLLECTIONS);
 
-    getCategoriesMap();
-  }, []);
+  console.log("Loading", loading);
+  console.log("Data", data);
+
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsData = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsData);
+    }
+  }, [data]);
 
   const value = { categoriesMap };
+
   return (
     <CategoriesContext.Provider value={value}>
       {children}
